9f252989ba66d545b2658824e63d6d7678d64ef0
anchor <protocol@hot1636.com>
2021-04-27 15:03:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# medianizer
+Due to the Medianizer having a static `wat` we need to have a custom contract for each one.
---
+++ src/median.sol
@@ -0,0 +1,31 @@
+/// median.sol
+
+// Copyright (C) 2019-2020 Maker Ecosystem Growth Holdings, INC.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >=0.5.10;
+
+import "median/median.sol";
+
+contract MedianHOTGBP is Median {
+    bytes32 public constant wat = "HOTGBP";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
